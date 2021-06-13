97ca95abcf952fd7a18babba9de3305c560bf1b4
sada <56000197+s-akhmedoff@users.noreply.github.com>
2021-06-13 17:16:45 +0500

    Update pkgs.sh

--- arch/pkgs.sh
+++ arch/pkgs.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Update gpg keys
+gpg --refresh-keys --keyserver hkps://keys.openpgp.org
+
 # Latest mirrors
 sudo reflector --country 'Russia,Kazakhstan' --protocol 'http,https' --latest 50 --sort rate --save /etc/pacman.d/mirrorlist
 
